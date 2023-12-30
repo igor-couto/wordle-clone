d970aecc6f316ed9258ea406978fd145daa119f6
Igor Couto <igor.fcouto@gmail.com>
2023-12-30 15:28:24 +0000

    Using collection expression to simplify initialization

--- src/Results/Result.cs
+++ src/Results/Result.cs
@@ -3,14 +3,14 @@ namespace WordleClone.Results;
 public abstract class Result
 {
     public bool IsCorrect { get; init; }
-    public Letter[] Letters { get; init; } = new Letter[5]
-    {
+    public Letter[] Letters { get; init; } =
+    [
         new (' ', LetterPlacement.NotPresent),
         new (' ', LetterPlacement.NotPresent),
         new (' ', LetterPlacement.NotPresent),
         new (' ', LetterPlacement.NotPresent),
         new (' ', LetterPlacement.NotPresent)
-    };
+    ];
     
     public string Message { get; init; }
 }
