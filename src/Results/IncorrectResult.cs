0d51f6db20fab4ffb89434ae8caacdb744c02ad2
Igor Couto <igor.fcouto@gmail.com>
2023-12-30 15:33:59 +0000

    fix letter placement logic

--- src/Results/IncorrectResult.cs
+++ src/Results/IncorrectResult.cs
@@ -1,20 +1,37 @@
+using System.Text;
+
 namespace WordleClone.Results;
 
 public class IncorrectResult : Result
 {
-    public IncorrectResult(string word, string correctWord)
+    public IncorrectResult(string wordGuess, string correctWord)
     {
         IsCorrect = false;
         Message = "One or more letters are correct";
 
-        for(var i = 0; i < correctWord.Length; i++)
+        var modifiableCorrectWord = new StringBuilder(correctWord);
+
+        for (var i = 0; i < wordGuess.Length; i++)
         {
-            if(word[i] == correctWord[i])
-                Letters[i] = new (word[i], LetterPlacement.Correct);
-            else if(correctWord.Contains(word[i])) //TODO: check this logic
-                Letters[i] = new (word[i], LetterPlacement.Incorrect);
+            var guessChar = wordGuess[i];
+
+            if (guessChar == correctWord[i])
+            {
+                Letters[i] = new Letter(guessChar, LetterPlacement.Correct);
+                modifiableCorrectWord[i] = '-';
+                continue;
+            }
+
+            var indexInModifiable = modifiableCorrectWord.ToString().IndexOf(guessChar);
+            if (indexInModifiable != -1)
+            {
+                Letters[i] = new Letter(guessChar, LetterPlacement.Incorrect);
+                modifiableCorrectWord[indexInModifiable] = '-';
+            }
             else
-                Letters[i] = new (word[i], LetterPlacement.NotPresent); 
+            {
+                Letters[i] = new Letter(guessChar, LetterPlacement.NotPresent);
+            }
         }
     }
 }
