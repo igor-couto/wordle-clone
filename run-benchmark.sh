8244daf1e146ee2a6241f5481019c9f409de9564
Igor Couto <igor.fcouto@gmail.com>
2024-01-02 16:02:30 +0000

    Fix run-benchmark script

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -5,9 +5,8 @@ BOLD='\033[1m'
 
 echo
 echo -e "${BOLD}Building in Release mode ...${NOCOLOR}"
-dotnet build -c Release
-
+dotnet build .\test\benchmark\Benchmark.csproj -c Release
 
 echo
 echo -e "${BOLD}Running benchmark ...${NOCOLOR}"
-dotnet [...] .dll
+dotnet .\test\benchmark\bin\Release\net8.0\Benchmark.dll
